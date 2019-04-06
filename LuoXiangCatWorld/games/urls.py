95d32618bb6e594ebb679a879b513e51f2be4efc
Josep-h <fdu-nus-2019@163.com>
2019-04-06 23:30:25 +0800

    update od views

--- LuoXiangCatWorld/games/urls.py
+++ LuoXiangCatWorld/games/urls.py
@@ -1,2 +1,13 @@
 from django.urls import path
 from . import views
+
+app_name = 'games'
+urlpatterns = [
+    path('', views.login, name='login'),
+    path('<int:master_id>/', views.detail, name='detail'),
+    path('<int:master_id>/cats/', views.cats, name='cats'),
+    path('<int:master_id>/parks/', views.parks, name='parks'),
+    path('<int:master_id>/parks/<int:park_id>/', views.park_detail, name='park_detail'),
+    path('<int:master_id>/sites/', views.sites, name='sites'),
+    path('<int:master_id>/markets/', views.markets, name='markets'),
+]
--- LuoXiangCatWorld/games/views.py
+++ LuoXiangCatWorld/games/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404,render
+from .models import Question, Choice
+from django.urls import reverse
 # Create your views here.
+from django.http import Http404
+from django.http import HttpResponse, HttpResponseRedirect
+
+
+# path('', views.login, name='login'),
+def login(request):
+    master_list = Master.objects.order_by('-pub_date')[:5]
+    context = {'master_list': master_list}
+    return render(request, 'games/login.html',context)
+
+# path('<int:master_id>/', views.detail, name='detail'),
+def detail(request,master_id):
+    master = get_object_or_404(Master, pk=master_id)
+    return render(request, 'games/detail.html',master)
+
+# path('<int:master_id>/cats/', views.cats, name='cats'),
+def cats(request,master_id):
+    cat_list = Cat.objects.order_by('-pub_date')[:5]
+    master = get_object_or_404(Master, pk=master_id)
+    context = {'cat_list': cat_list,'master':master}
+    return render(request, 'games/cats.html',context)
+
+# path('<int:master_id>/parks/', views.parks, name='parks'),
+def parks(request,master_id):
+    #TODO
+    return render(request, 'games/parks.html')
+
+# path('<int:master_id>/parks/<int:park_id>/', views.park_detail, name='park_detail'),
+def park_detail(request,master_id,park_id):
+    #TODO
+    return render(request, 'games/park_detail.html')
+
+# path('<int:master_id>/sites/', views.sites, name='sites'),
+def sites(request,master_id):
+    #TODO
+    return render(request, 'games/sites.html')
+
+# path('<int:master_id>/markets/', views.markets, name='markets'),
+def markets(request,master_id):
+    market_list = Market.objects.order_by('-pub_date')[:5]
+    master = get_object_or_404(Master, pk=master_id)
+    context = {'market_list': market_list,'master':master}
+    return render(request, 'games/markets.html',context)
+
+def sign_up(request):
+    #TODO
+    return render(request, 'games/sign_up.html')

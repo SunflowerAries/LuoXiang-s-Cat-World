68e35664443979c8444dd0e9cc4fdf64fb08992c
Josep-h <fdu-nus-2019@163.com>
2019-04-09 23:55:41 +0800

    crazy...

---
+++ LuoXiangCatWorld/games/static/games/material.js
@@ -0,0 +1,5 @@
+const select = new MDCSelect(document.querySelector('.mdc-select'));
+
+select.listen('MDCSelect:change', () => {
+  alert(`Selected option at index ${select.selectedIndex} with value "${select.value}"`);
+});
